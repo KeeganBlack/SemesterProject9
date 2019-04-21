13c06447296ea7456764d934968ed79949a99d6b
Logan Harris <harris.logan@outlook.com>
2019-04-21 08:24:15 -0400

    Initial commit with project files. Addition of HomeView, navigation controller, and SwiftLibObj

---
+++ SemsterProject9/SemsterProject9/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  homeView.swift
+//  SemsterProject9
+//
+//  Created by Logan Harris on 4/21/19.
+//  Copyright © 2019 SemesterProject9. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SemsterProject9/SemsterProject9/SwiftLibObj.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftLibObj.swift
+//  SemsterProject9
+//
+//  Created by Logan Harris on 4/21/19.
+//  Copyright © 2019 SemesterProject9. All rights reserved.
+//
+
+import Foundation
+
+class SwiftLibObj {
+    
+    let arguments : [String] = [];
+    let story : [String] = [];
+    
+}
