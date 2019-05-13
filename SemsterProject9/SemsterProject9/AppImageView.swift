d8407eb1d9dd573bf4dd75bce4047d5b61a585e9
Deepti Konduru <deeptikonduru@wireless-10-105-58-254.umd.edu>
2019-05-13 15:50:48 -0400

    Changes to storyboard:Deepti

---
+++ SemsterProject9/SemsterProject9/AppImageView.swift
@@ -0,0 +1,15 @@
+//
+//  AppImageView.swift
+//  SemsterProject9
+//
+//  Created by Deepti Konduru on 5/13/19.
+//  Copyright © 2019 SemesterProject9. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppImageView: UIImageView {
+    
+    
+}
