8334c55759bf9fe97d7098cdf0cff6d4d8e92175
Deepti Konduru <deeptikonduru@wireless-10-104-55-126.umd.edu>
2019-05-14 11:44:14 -0400

    Added animation to buttons at the home page: Deepti

--- SemsterProject9/SemsterProject9/ViewController.swift
+++ SemsterProject9/SemsterProject9/ViewController.swift
@@ -10,14 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var AppIconImage: UIImageView!
+    @IBOutlet weak var AppTitleLabel: UILabel!
+    
+
     
     @IBOutlet var ButtonCollection: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        AppTitleLabel.textColor = UIColor.blue
+        for button in ButtonCollection {
+            button.backgroundColor = UIColor.blue
+            button.layer.cornerRadius = 5
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.black.cgColor
+            UIView.animate(withDuration: 1, animations: {
+                button.frame.size.width += 10
+                button.frame.size.height += 10
+            }) {
+                _ in
+                UIView.animate(withDuration: 1, delay: 0.25, options: [.autoreverse, .repeat], animations:{ button.frame.origin.y -= 20})
+                
+            }
+        }
         
+       
     }
 
+}
+
+extension UIColor{
+    
 }
 
