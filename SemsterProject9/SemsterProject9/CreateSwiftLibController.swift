a425b259d42bc2af2c21dc5b13f5bac7a65c15e3
Logan Harris <harris.logan@outlook.com>
2019-05-14 22:01:32 -0400

    Logan: Implementation of regex

--- SemsterProject9/SemsterProject9/CreateSwiftLibController.swift
+++ SemsterProject9/SemsterProject9/CreateSwiftLibController.swift
@@ -10,6 +10,16 @@ import Foundation
 import UIKit
 import Firebase
 
+extension String {
+    func split(usingRegex pattern: String) -> [String] {
+        //### Crashes when you pass invalid `pattern`
+        let regex = try! NSRegularExpression(pattern: pattern)
+        let matches = regex.matches(in: self, range: NSRange(0..<utf16.count))
+        let ranges = [startIndex..<startIndex] + matches.map{Range($0.range, in: self)!} + [endIndex..<endIndex]
+        return (0...matches.count).map {String(self[ranges[$0].upperBound..<ranges[$0+1].lowerBound])}
+    }
+}
+
 class CreateSwiftLibController : UIViewController {
     
     var args : [String] = []
@@ -39,7 +49,10 @@ class CreateSwiftLibController : UIViewController {
     let rootRef = Database.database().reference()
     
     func splitStory(story: String) -> [String] {
-        return story.components(separatedBy: " ")
+        let str = story
+        let separator = "{\\w+}"
+        let result = str.split(usingRegex: separator)
+        return result
     }
     
     @IBAction func SubmitSwiftLib(_ sender: Any) {
