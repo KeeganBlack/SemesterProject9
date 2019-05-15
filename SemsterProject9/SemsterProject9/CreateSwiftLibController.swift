c05085ff4663bf60ed7d16c5701b12f07834c36a
Keegan Black <bkeeganp@gmail.com>
2019-05-14 20:39:32 -0400

    Keegan: Added code for crete new lib in database

--- SemsterProject9/SemsterProject9/CreateSwiftLibController.swift
+++ SemsterProject9/SemsterProject9/CreateSwiftLibController.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import UIKit
-
+import Firebase
 
 class CreateSwiftLibController : UIViewController {
     
@@ -20,6 +20,8 @@ class CreateSwiftLibController : UIViewController {
     @IBOutlet weak var ClearButtonOutlet: UIButton!
     @IBOutlet weak var SubmitButtonOutlet: UIButton!
     @IBOutlet var ActionButtons: [UIButton]!
+    
+    let rootRef = Database.database().reference()
     
     @IBAction func InsertBlank(_ sender: Any) {
         self.SwiftLibStory.text = self.SwiftLibStory.text + "{BLANK}"
@@ -35,6 +37,13 @@ class CreateSwiftLibController : UIViewController {
     
     @IBAction func ClearTextAction(_ sender: Any) {
         self.SwiftLibStory.text = ""
+    }
+    
+    func saveToFirebase(lib : SwiftLibObj) {
+        let usersRef = rootRef.child("Users")
+        let user = usersRef.child(lib.author).child("SwiftLibTemplates")
+        let values = ["Title":lib.title, "Score": 0, "Arguments": lib.arguments, "Story": lib.story] as [String : Any]
+        user.childByAutoId().setValue(values)
     }
     
 }
